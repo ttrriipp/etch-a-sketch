1dbf61f880ad3e55679b11c3fbb71e27fbbe96cc
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-14 09:27:52 +0800

    Fix row and columnt count variables

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const container = document.querySelector('.container');
 const COLUMN_COUNT = 16;
 const ROW_COUNT = 16;
 
-for (let i = 0; i < COLUMN_COUNT; i++) {
+for (let i = 0; i < ROW_COUNT; i++) {
     const rowBoxContainer = document.createElement("div");
     rowBoxContainer.setAttribute('class', 'row-box-container');
     rowBoxContainer.classList.add('row-box-container')
     container.appendChild(rowBoxContainer);
-    
-    for (let j = 0; j < ROW_COUNT; j++) {
+
+    for (let j = 0; j < COLUMN_COUNT; j++) {
         const box = document.createElement("div");
         box.setAttribute('class', 'box');
         box.classList.add('box');
