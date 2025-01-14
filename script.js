8b3f82d2f3408a62194b9ee1fab63761f8f63c64
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-14 10:27:49 +0800

    Move make grid and hover effect into functions

--- script.js
+++ script.js
@@ -2,25 +2,48 @@ const container = document.querySelector('.container');
 
 const COLUMN_COUNT = 16;
 const ROW_COUNT = 16;
+let gridSize = 0;
 
-for (let i = 0; i < ROW_COUNT; i++) {
+const button = document.createElement("button");
+container.appendChild(button);
+button.style.height = "500px";
+button.textContent = "new grid";
+button.addEventListener('click', () => {
+    while (gridSize > 100 || gridSize < 0) {
+        gridSize = prompt("size of grid?");
+    } 
+    if (gridSize != null) {
+        makeGrid(gridSize, gridSize);
+    }
+});
+
+makeGrid(COLUMN_COUNT, ROW_COUNT);
+hoverEffect();
+
+function hoverEffect() {
+    const box = document.querySelectorAll('.box');
+    box.forEach (cell => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'blue';
+        });
+    });
+}
+
+
+function makeGrid(column, row) {
+    for (let i = 0; i < row; i++) {
     const rowBoxContainer = document.createElement("div");
     rowBoxContainer.setAttribute('class', 'row-box-container');
     rowBoxContainer.classList.add('row-box-container')
     container.appendChild(rowBoxContainer);
 
-    for (let j = 0; j < COLUMN_COUNT; j++) {
-        const box = document.createElement("div");
-        box.setAttribute('class', 'box');
-        box.classList.add('box');
-        rowBoxContainer.appendChild(box);
+        for (let j = 0; j < column; j++) {
+            const box = document.createElement("div");
+            box.setAttribute('class', 'box');
+            box.classList.add('box');
+            rowBoxContainer.appendChild(box);
+        }
     }
 }
 
-const box = document.querySelectorAll('.box');
 
-box.forEach (cell => {
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'blue';
-    });
-});
