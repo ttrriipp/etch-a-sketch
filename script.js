6449372299414b127bc53c311f8f6d8ee01dccff
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-14 09:25:27 +0800

    Create 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const container = document.querySelector('.container');
+
+const COLUMN_COUNT = 16;
+const ROW_COUNT = 16;
+
+for (let i = 0; i < COLUMN_COUNT; i++) {
+    const rowBoxContainer = document.createElement("div");
+    rowBoxContainer.setAttribute('class', 'row-box-container');
+    rowBoxContainer.classList.add('row-box-container')
+    container.appendChild(rowBoxContainer);
+    
+    for (let j = 0; j < ROW_COUNT; j++) {
+        const box = document.createElement("div");
+        box.setAttribute('class', 'box');
+        box.classList.add('box');
+        rowBoxContainer.appendChild(box);
+    }
+}
